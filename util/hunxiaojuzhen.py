afa7f7bd182be439f62ab1117a568f5fa719cc00
magic_xianyi <xianyi_zhou@163.com>
2024-02-28 10:48:21 +0800

    消融实验

--- util/hunxiaojuzhen.py
+++ util/hunxiaojuzhen.py
@@ -9,16 +9,18 @@ if __name__ == '__main__':
     FP = 875   #假阳
     FN = 2971  #假阴
     TN = 97621 #真阴
-    #precision = TP / (TP + FP)
-    #recall = TP / (TP + FN)
-    #f1_score = 2 * precision * recall / (precision + recall)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1_score = 2 * precision * recall / (precision + recall)
+
+    print("recall: {} \nprecision: {} \nf1_score: {}".format(recall, precision, f1_score))
     f1_score = np.copy([0.8421, 0.8077, 0.8254, 0.8571])
     precision = np.copy([0.8897, 0.9475, 0.9423, 0.9295])
     recall = np.empty(4)
     for i in range(4):
         recall[i] = f1_score[i] * precision[i] / (2 * precision[i] - f1_score[i])
 
-    #print("recall: {} \nprecision: {} \nf1_score: {}".format(recall, precision, f1_score))
+    print("recall: {} \nprecision: {} \nf1_score: {}".format(recall, precision, f1_score))
     # 创建混淆矩阵
     confusion_matrix = np.array([[TN, FP],
                                  [FN, TP]])
--- util/xiaorong.py
+++ util/xiaorong.py
@@ -1,22 +1,27 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置字体中文乱码问题
 plt.rcParams["axes.unicode_minus"] = False  # 该语句解决图像中的“-”负号的乱码问题
 if __name__ == '__main__':
-    data = [[66386, 174296, 75131, 577908, 32015],
-            [58230, 381139, 78045, 99308, 160454],
-            [89135, 80552, 152558, 497981, 603535],
-            [78415, 81858, 150656, 193263, 69638]]
-    columns = ('Model 1', 'Model 2', 'Model 3', 'Model 4', 'Model 5')
-    rows = ['Metric %d' % x for x in (1, 2, 3, 4)]
+    #去除gru模块各各参数均有下降，有效但不是最重要因素。
+    #去除ISOA模块precision下降最多，因为无关变量的影响大了，导致检测误报率变高了
+    #阈值模块这里使用普通的阈值方式，即使用训练经验得到的固定阈值，去除阈值模块后各指标都是最低，因为参考单一并且没有动态阈值
+    precision = np.copy([0.9295, 0.8862, 0.9171, 0.8931])
+    f1_socre = np.copy([0.8571, 0.82531, 0.8322, 0.8123])
+    recall = np.empty(4)
+    for i in range(4):
+        recall[i] = format(f1_socre[i] * precision[i] / (2 * precision[i] - f1_socre[i]), '.4f')
+    data = [recall, precision, f1_socre]
+    columns = ('本章模型', '去除ISOA模块', '去除GRU模块', '去除阈值模块')
+    rows = ['Recall', 'Precision', 'F1_Score']
     pltPD = pd.DataFrame(data, columns=columns, index=rows)
     # 按照model来进行汇总, 这里共有5个model
-    fig = plt.figure(figsize=(12, 8))
+    fig = plt.figure(figsize=(12, 10))
     ax = fig.add_subplot(1, 1, 1)
     # 自定义颜色
     # colors = plt.cm.BuPu(np.linspace(0, 0.5, len(pltPD.index.values)))
-    colors = plt.cm.rainbow(np.linspace(0, 1, len(pltPD.index.values)))  # plasma, rainbow, jet
+    colors = ['gainsboro', 'wheat','lightsteelblue']
     n_rows = len(pltPD.index.values)
     # 设置barchart的x轴
     w = 0.3
@@ -26,14 +31,10 @@ if __name__ == '__main__':
         plt.bar(index + row * w, pltPD.values[row], width=w, align='center', color=colors[row],
                 label=pltPD.index.values[row])
     # 添加图例
-    ax.legend(fontsize=12)
-    # 矩阵的颜色高亮
-    # norm = plt.Normalize(0, 1)
-    # cellColours = plt.cm.Blues(norm(pltPD.values))
-    # 使用最大最小标准化, 使得颜色区分更大
-    minmax = (pltPD.values - pltPD.values.min(1, keepdims=True)) / (
-                pltPD.values.max(1, keepdims=True) - pltPD.values.min(1, keepdims=True))
-    cellColours = plt.cm.Blues(minmax)
+    ax.legend(fontsize=16)
+    cellColours = [['gainsboro', 'white', 'gainsboro', 'white'],
+                   ['white', 'wheat', 'white', 'wheat'],
+                   ['lightsteelblue', 'white', 'lightsteelblue', 'white']]
     # 添加一个表格在底部
     the_table = plt.table(cellText=pltPD.values,
                           cellColours=cellColours,
@@ -42,14 +43,10 @@ if __name__ == '__main__':
                           colLabels=pltPD.columns.values,
                           loc='bottom',
                           cellLoc='center')
-    the_table.set_fontsize(14)
-    the_table.scale(1.0, 1.7)  # may help
-    # 调整table的布局
-    # plt.subplots_adjust(left=0.2, bottom=0.1)
-    # label和ticks的设置
-    # minN, maxN = pltPD.values.flatten().min(), pltPD.values.flatten().max()
-    # plt.yticks(np.arange(minN, maxN, (maxN-minN)/5), fontsize=14)
-    plt.yticks(np.arange(0, 1.1, 0.2), fontsize=14)
+    the_table.set_fontsize(15)
+    the_table.scale(1, 2.5)  # may help
     plt.xticks([])
-    plt.title('Comparison between Different Models', fontsize=14)
+    plt.yticks(fontsize=15)
+    plt.ylim(0.4, 1.2)
+    plt.title('ISBGAT消融测试数据', fontsize=18)
     plt.show()
